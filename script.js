52d4878a45dbc2af2cab938a1a402c13fcd40753
Petra Vicherkova <peta.vicherkova@gmail.com>
2024-06-01 22:15:59 +0200

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const justFood = (peopleAmount) => {  
+    cateringName = "Just Food"    
+    cateringprice = 300
+}
+
+const yourMama = (peopleAmount) => {
+    cateringName = "Your Mama"    
+    cateringprice = 1000   
+}
+
+const flavourHaven = (peopleAmount) => {
+    cateringName = "Flavour Haven"    
+    cateringprice = 3000
+}
+
+const createEvent = (eventName, peopleAmount, catering) => {
+    return document.querySelector(".hlaska").innerHTML = "Událost " + eventName + " s cateringem od " + cateringName + " pro " + peopleAmount + " lidí za " + cateringprice*peopleAmount + " Kč."
+}
+
+createEvent("Narozeniny", 10, justFood())
